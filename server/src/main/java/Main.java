07d81e124c3190da8be06148b3077172e002bbff
zackjsorensen <zackjsorensen@gmail.com>
2024-05-24 14:58:46 -0600

    Dummy Endpoint works when Server started from Main

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,26 +2,49 @@ import Service.UserService;
 import chess.*;
 import dataaccess.MemoryUserDAO;
 import server.Server;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Server: " + piece);
-
         try {
             var port = 8080;
             if (args.length >= 1) {
                 port = Integer.parseInt(args[0]);
             }
+            // create a server - something like this
+            Server server = new Server();
+            server.run(port);
+//            var port = server.run(port);
+//            System.out.println("Started test HTTP server on " + port);
+//            createRoutes();
 
-            var service = new UserService(new MemoryUserDAO());
-            var server = new Server(service); // .run(port)
-//            port = server.port();
-            System.out.printf("Server started on port %d%n", port);
-            return;
-        } catch (Throwable ex) {
-            System.out.printf("Unable to start server: %s%n", ex.getMessage());
-
+//            Spark.awaitInitialization();
+            System.out.println("Listening on port " + port);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            System.err.println("Specify the port number as a command line parameter");
         }
     }
 }
+
+//    private static void createRoutes() {
+//        Spark.get("/hello", (req, res) -> "Hello Zacharias!");
+//    }
+
+
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+
+
+//
+//            var service = new UserService(new MemoryUserDAO());
+//            var server = new Server();
+//            server.run(port);
+//
+//            System.out.printf("Server started on port %d%n", port);
+//            return;
+//        } catch (Throwable ex) {
+//            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+//
+//        }
+//    }
+//}
--- server/src/main/java/Service/UserService.java
+++ server/src/main/java/Service/UserService.java
@@ -1,26 +1,26 @@
-//package Service;
-//
-//import dataaccess.MemoryUserDAO;
-//import model.UserData;
-//
-//public class UserService {
-//    private final MemoryUserDAO dataAccess;
-//    // this is the DAO object created by main where data is stored while the service is running
-//
-//    public UserService(MemoryUserDAO dataAccess) {
-//        this.dataAccess = dataAccess;
-//    }
-//
-//    public UserData getUser(String username){
-//        return (UserData) dataAccess.get(username);
-//    }
-//
-//    public void addUser(UserData user){
-//        dataAccess.add(user);
-//    }
-//
-//    public void clearUsers(){
-//        dataAccess.clear();
-//    }
-//
-//}
+package Service;
+
+import dataaccess.MemoryUserDAO;
+import model.UserData;
+
+public class UserService {
+    private final MemoryUserDAO dataAccess;
+    // this is the DAO object created by main where data is stored while the service is running - starts out empty
+
+    public UserService(MemoryUserDAO dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public UserData getUser(String username){
+        return (UserData) dataAccess.get(username);
+    }
+
+    public void addUser(UserData user){
+        dataAccess.add(user);
+    }
+
+    public void clearUsers(){
+        dataAccess.clear();
+    }
+
+}
