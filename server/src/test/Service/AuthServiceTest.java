8722f855012dee4e2dda24e973a8cfd8804c48fc
zackjsorensen <zackjsorensen@gmail.com>
2024-05-25 17:47:43 -0600

    AuthService is passing tests

--- server/src/test/Service/AuthServiceTest.java
+++ server/src/test/Service/AuthServiceTest.java
@@ -18,6 +18,15 @@ class AuthServiceTest {
     }
 
     @Test
+    void clear(){
+        String keithAuth = service.createAuth("Keith");
+        String bobAuth = service.createAuth("Bob");
+        service.clear();
+        assertNull(service.getAuth(keithAuth));
+        assertNull(service.getAuth(bobAuth));
+    }
+
+    @Test
     void getFromEmptyAuth() {
         Assertions.assertNull(service.getAuth("0000"));
     }
@@ -26,5 +35,20 @@ class AuthServiceTest {
     void getAuthBob(){
         String bobAuth = service.createAuth("Bob");
         assertEquals("Bob", service.getAuth(bobAuth).username());
+        assertNotEquals("Bobo", service.getAuth(bobAuth).username());
+    }
+
+    @Test
+    void deleteEmptyAuth(){
+        service.deleteAuth("1234");
+        assertNull(service.getAuth("1234"));
+    }
+
+    @Test
+    void deleteAuth(){
+        String joeAuth = service.createAuth("Joe");;
+        assertNotNull(service.getAuth(joeAuth));
+        service.deleteAuth(joeAuth);
+        assertNull(service.getAuth(joeAuth));
     }
 }
