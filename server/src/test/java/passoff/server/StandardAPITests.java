325b1deb48239d1c4275099eb272a19034db4d17
zackjsorensen <zackjsorensen@gmail.com>
2024-05-27 19:31:00 -0600

    Working on error handling for login

--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -1,7 +1,8 @@
+package passoff.server;
+
 import chess.ChessGame;
 import org.junit.jupiter.api.*;
 import passoff.model.*;
-import passoff.server.TestServerFacade;
 import server.Server;
 
 import java.net.HttpURLConnection;
