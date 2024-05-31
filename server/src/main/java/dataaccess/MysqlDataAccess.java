e9f558d9496363cbce95280d1f61004f5d9218e2
zackjsorensen <zackjsorensen@gmail.com>
2024-05-31 14:41:52 -0600

    Phase 4 init: sqlDA class with stubs, dbConfig

---
+++ server/src/main/java/dataaccess/MysqlDataAccess.java
@@ -0,0 +1,24 @@
+package dataaccess;
+
+
+
+public class MysqlDataAccess implements DataAccessInterface{
+
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void add(Object dataObj) {
+
+    }
+
+    @Override
+    public Object get(Object identifier) {
+        return null;
+    }
+
+    private void configureDatabase()
+}
