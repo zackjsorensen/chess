730630bdbd5fe9e601185ccfe20aef2e01f2bcda
zackjsorensen <zackjsorensen@gmail.com>
2024-05-31 14:50:55 -0600

    Phase 4: create statement started, safety commit

--- server/src/main/java/dataaccess/MysqlDataAccess.java
+++ server/src/main/java/dataaccess/MysqlDataAccess.java
@@ -4,6 +4,8 @@ package dataaccess;
 
 public class MysqlDataAccess implements DataAccessInterface{
 
+    public MysqlDataAccess() {
+    }
 
     @Override
     public void clear() {
@@ -20,5 +22,31 @@ public class MysqlDataAccess implements DataAccessInterface{
         return null;
     }
 
-    private void configureDatabase()
+    private final String[] createStatements = {
+            // not sure if I need ID here.. only reason would be to have foreign keys...
+            """
+            CREATE TABLE IF NOT EXISTS users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256),
+              'email' varchar(256),
+              PRIMARY KEY (`username`),
+              INDEX(type), // vas es das? 
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws ResponseException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
