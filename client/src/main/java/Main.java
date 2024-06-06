4e7b1f44342ca9b0503b9606fa49edffb1466bd0
zackjsorensen <zackjsorensen@gmail.com>
2024-06-06 10:44:33 -0600

    Phase 5: Blank Chessboard for white or black turn

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -3,10 +3,10 @@ import chess.ChessPiece;
 import ui.DrawChessBoard;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-        DrawChessBoard draw = new DrawChessBoard();
+        DrawChessBoard draw = new DrawChessBoard(new ChessGame(), "WHITE");
         draw.drawAll();
     }
 }
--- client/src/main/java/ui/DrawChessBoard.java
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -2,68 +2,105 @@ package ui;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+
+import chess.ChessGame;
 import ui.EscapeSequences;
 
 public class DrawChessBoard {
     static PrintStream out;
-    EscapeSequences es;
     private static final int SQUARE_SIZE_IN_CHARS = 3;
     int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
     int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
     private static final String EMPTY = "   ";
+    String letters;
+    ChessGame game;
+    String color;
+    boolean whiteLeadsOnEven;
 
-    public DrawChessBoard() {
+    public DrawChessBoard(ChessGame game, String color) throws Exception {
         out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        es = new EscapeSequences();
+        this.game = game;
+        this.color = color;
+        if (color.equals("BLACK")) {
+            whiteLeadsOnEven = true;
+        } else if (color.equals("WHITE")) {
+            whiteLeadsOnEven = false;
+        } else {
+            throw new Exception("Bad color parameter");
+        }
     }
 
-    public void drawAll(){
+
+    public void drawAll() {
+        if (color.equals("BLACK")) {
+            drawBlackTurn();
+        } else {
+            drawWhiteTurn();
+        }
+    }
+
+    private void drawBlackTurn() {
+        letters = "    a  b  c  d  e  f  g  h";
         drawRows();
     }
 
-    private void drawLetters(){
-        out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
-        out.print(EscapeSequences.SET_TEXT_COLOR_WHITE);
-        out.println("    a  b  c  d  e  f  g  h");
+    private void drawWhiteTurn() {
+        letters = "    h  g  f  e  d  c  b  a";
+        drawRows();
     }
 
-    private void drawRows(){
+    private void drawLetters() {
+        out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+        out.print(EscapeSequences.SET_TEXT_COLOR_WHITE);
+        out.println(letters);
+    }
+
+    private void drawRows() {
         drawLetters();
-        for (int row = 0; row < 8; row++){
+        for (int row = 0; row < 8; row++) {
             out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
-            out.printf(" %s ", (8-row));
-            if (row % 2 == 0){
+            drawNumbers(row);
+            if (row % 2 == 0) {
                 drawWhiteRow();
             } else {
                 drawBlackRow();
             }
+
             out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
-            out.printf(" %s ", (8-row));
+            drawNumbers(row);
             out.println();
         }
         drawLetters();
     }
 
+    private void drawNumbers(int row) {
+        if (color.equals("WHITE")){
+            out.printf(" %s ", (row + 1));
+        } else {
+            out.printf(" %s ", (8 - row));
+        }
+    }
+
     private void drawWhiteRow() {
-       for (int column = 0; column < 8; column = column + 2){
+        for (int column = 0; column < 8; column = column + 2) {
             drawWhiteSquare();
             drawBlackSquare();
-       }
+        }
     }
 
     private void drawBlackRow() {
-        for (int column = 0; column < 8; column = column + 2){
+        for (int column = 0; column < 8; column = column + 2) {
             drawBlackSquare();
             drawWhiteSquare();
         }
     }
 
-    private void drawWhiteSquare(){
+    private void drawWhiteSquare() {
         out.print(EscapeSequences.SET_BG_COLOR_WHITE);
         out.print(EMPTY);
     }
 
-    private void drawBlackSquare(){
+    private void drawBlackSquare() {
         out.print(EscapeSequences.SET_BG_COLOR_BLACK);
         out.print(EMPTY);
     }
