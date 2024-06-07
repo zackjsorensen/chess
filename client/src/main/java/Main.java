be693e730b8d3471ab5bf1dd9f606135a945e7d8
zackjsorensen <zackjsorensen@gmail.com>
2024-06-07 12:15:51 -0600

    Phase 5: Menus started, text prompts and receiving user input for quit, help, and register

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,12 +2,44 @@ import chess.ChessGame;
 import chess.ChessPiece;
 import ui.DrawChessBoard;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-        DrawChessBoard draw = new DrawChessBoard(new ChessGame(), "BLACK");
-        draw.drawAll();
+
+        System.out.println("♕ Welcome to 240 Chess. Type help for list of commands.");
+        PrintStream out = new PrintStream(System.out, true,StandardCharsets.UTF_8);
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while(!line.equalsIgnoreCase("quit")) {
+            if (line.equalsIgnoreCase("help")) {
+                // call help method, pass out as param
+                help(out);
+            } else if (line.equalsIgnoreCase("register")) {
+                register(out, scanner);
+            }
+            line = scanner.nextLine();
+        }
+
+    }
+
+    private static void help(PrintStream out){
+        out.println("Commands:");
+        out.println(" - register: create a new user");
+        out.println(" - login: kind of self-explanatory");
+        out.println(" - quit: also self-explanatory");
+        out.println(" - help: see this exact same message, again! You should choose this option...");
+    }
+
+    private static void register(PrintStream out, Scanner scanner){
+        out.println("Enter a username");
+        String username = scanner.nextLine();
+        out.println("Enter a password");
+        String password = scanner.nextLine();
+        out.println("Enter an email address (optional)");
+        String email = scanner.nextLine();
     }
 }
 
